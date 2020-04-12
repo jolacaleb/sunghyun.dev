541e9deafdc25eb615a8a3fddc759e93f0ae8264
sunghyun-lee <jolacaleb@gmail.com>
2020-04-13 05:10:53 +0900

    Update `about`
    - Use `material-ui` and decorated ... some places
    - I don't care if it is beautiful or not, i just did.

---
+++ content/components/SkillBar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { lighten, makeStyles, withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Tooltip from '@material-ui/core/Tooltip';
+
+function SkillBar(props) {
+    const classes = makeStyles((theme) => ({
+        root: {
+          flexGrow: 1,
+        },
+        margin: {
+          margin: theme.spacing(1),
+        },
+      }));
+    const BorderLinearProgress = withStyles({
+        root: {
+          height: 10,
+          backgroundColor: lighten(props.color, 0.5),
+        },
+        bar: {
+          borderRadius: 20,
+          backgroundColor: props.color
+        },
+      })(LinearProgress);
+  return (
+    <Tooltip title={props.caption} placement="right">
+        <div style={{'width': '50%', 'margin-bottom': '16px'}}>
+            <span>{props.name}</span>
+                <BorderLinearProgress 
+                    className={classes.margin}
+                    variant="determinate" 
+                    color="primary"
+                    value={props.value}
+                />
+        </div>
+    </Tooltip>
+  );
+}
+
+export default SkillBar;
